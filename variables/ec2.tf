330a7434a6b9258d757d21bf9cdd6fbb81b13cfe
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-28 11:57:35 +0530

    variables

---
+++ variables/ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "expense" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags                   = var.ec2_tags
+}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  description = "allowing ssh"
+  ingress {
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_blocks
+  }
+  tags = var.sg_tag
+}
---
+++ variables/variables.tf
@@ -0,0 +1,34 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-09c813fb71547fc4f"
+}
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+variable "ec2_tags" {
+  type = map(any)
+  default = {
+    Name        = "expense-demo"
+    Environment = "dev"
+    component   = "frontend"
+  }
+}
+variable "from_port" {
+  type    = number
+  default = 22
+}
+variable "to_port" {
+  type    = number
+  default = 22
+}
+variable "cidr_blocks" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+variable "sg_tag" {
+  type = map 
+  default = {
+    Name = "sg-demo"
+  }
+}
